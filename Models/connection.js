a3255feb8cc85b9311151aa65e97ffbd76839093
justbeenam <hoangngvietnam@gmail.com>
2023-08-28 18:23:51 +0700

    update

--- Models/connection.js
+++ Models/connection.js
@@ -1,14 +1,26 @@
-const { Sequelize, DataTypes } = require('sequelize')
+const { Sequelize, DataTypes } = require("sequelize");
+const fs = require("fs");
+
+const serverCa = [
+  fs.readFileSync("../../DigiCertGlobalRootCA.crt.pem", "utf8"),
+];
+
 const {
   DB_NAME,
   DB_HOST,
   DB_USERNAME,
   DB_PASSWORD,
   DB_DIALECT,
-} = require('../Config/db.config')
+} = require("../Config/db.config");
 
 const connection = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   dialect: DB_DIALECT,
-})
-module.exports = { connection }
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true,
+      ca: serverCa,
+    },
+  },
+});
+module.exports = { connection };
